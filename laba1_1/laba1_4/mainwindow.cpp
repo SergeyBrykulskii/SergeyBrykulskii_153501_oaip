c1d7bb14c38c34c2a2b67cb53f6d1b6278f34dfd
SergeyBrykulskii <serxio6758sk@gmail.com>
2022-03-05 11:22:31 +0300

    addition to 4 tast (1)

--- laba1_1/laba1_4/mainwindow.cpp
+++ laba1_1/laba1_4/mainwindow.cpp
@@ -1,13 +1,56 @@
 #include "mainwindow.h"
 
+QString FIRMS[4]{ "Intel", "HP", "MSI", "ASUS"};
+QString TYPE[5]{ "Processor", "Motherboard", "RAM", "Disk drive", "Case"};
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    ui.radioButtonStock->setChecked(true);
     ui.tableWidget;
+
+    size = 1;
+    ComputerParts* itemOfCP = new ComputerParts[size];
 }
 
 void MainWindow::on_OpenFile_clicked()
 {
+    FilePath = QFileDialog::getOpenFileName(this, "Choose file", "C:/", "(*.txt);");
 
+    QFile file(FilePath);
+
+    file.seek(0);
+    file.open(QFile::ReadOnly);
+    
+    QTextStream fin(&file);
+    QString checkData = fin.readLine();
+    
+    if (checkData != "Intel" && checkData != "HP" && checkData != "MSI" && checkData != "ASUS")
+    {
+        QMessageBox::critical(this, "Warning", "Please, choose correct file");
+        FilePath = "";
+    }
+}
+
+void MainWindow::on_ShowInfo_clicked()
+{
+    if (FilePath == "")
+    {
+        QMessageBox::critical(this, "Warning", "Please, choose correct file");
+    }
+    else
+    {
+        
+    }
+}
+
+void MainWindow::on_Add_clicked()
+{
+    
+}
+
+void MainWindow::on_Exit_clicked()
+{
+    close();
 }
--- laba1_1/laba1_4/mainwindow.h
+++ laba1_1/laba1_4/mainwindow.h
@@ -4,6 +4,7 @@
 #include <QFileDialog>
 #include <QFile>
 #include <QMessageBox>
+#include <QTextStream>
 
 #include "ui_mainwindow.h"
 #include "computerParts.h"
@@ -19,7 +20,14 @@ private slots:
 
     void on_OpenFile_clicked();
 
+    void on_Exit_clicked();
+
+    void on_Add_clicked();
+
+    void on_ShowInfo_clicked();
+
 private:
     Ui::MainWindowClass ui;
+    static int size;
     QString FilePath;
 };
